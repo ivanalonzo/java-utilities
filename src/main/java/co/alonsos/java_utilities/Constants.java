59dbfa212298d27319d5597d4890904f263d27d9
Ivan Alonso <ivan@alonso.tv>
2018-03-14 12:58:18 +0100

    Including methods for dealing with strings and test data

---
+++ src/main/java/co/alonsos/java_utilities/Constants.java
@@ -0,0 +1,41 @@
+package co.alonsos.java_utilities;
+
+import java.util.HashMap;
+
+public class Constants {
+	
+	/**
+	 * A static method which returns a map of conversion strings
+	 * for use by cleanup methods
+	 * @return
+	 */
+	public static final HashMap<String, String> cleanupMap(){
+		HashMap<String,String> map =new HashMap<String,String>();  
+		//Look for hex encoded chars and replace them 
+		map.put("&#x2014;", " - ");
+		map.put("&#x201D;", "\"");
+		map.put("&#x201C;", "\"");
+		map.put("&#x2018;", "'");
+		map.put("&#x2019;", "'");
+		
+		//Look for HTML encoded chars and replace them
+		map.put("&quot;", "\"");
+		map.put("&apos;", "'");
+		map.put("&amp;", "&");
+		return map;
+		
+	}
+	
+	/**
+	 * A static method which returns a map of ASCII to HTML tags for use 
+	 * by a conversion method
+	 * @return
+	 */
+	public static final HashMap<String, String> toHTMLMap(){
+		HashMap<String,String> map =new HashMap<String,String>();  
+		map.put("\\n", "<br>");
+		map.put("\\s\\s", "<br>");
+		return map;
+		
+	}
+}
--- src/main/java/co/alonsos/java_utilities/io/IO_Text.java
+++ src/main/java/co/alonsos/java_utilities/io/IO_Text.java
@@ -1,6 +1,16 @@
 package co.alonsos.java_utilities.io;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.apache.log4j.Logger;
+
+import co.alonsos.java_utilities.Constants;
+
+
 public class IO_Text {
+	private static Logger log = Logger.getLogger(IO_Text.class);
 
 	/**
 	 * Given the input, it will replace strings with their HTML counterparts. It currently has the
@@ -10,14 +20,71 @@ public class IO_Text {
 	 * @param input
 	 * @return
 	 */
+	@SuppressWarnings("rawtypes")
 	public String convertToHTML(String input) throws Exception {
 		String converted = "";
-		input = input.replaceAll("\\n", "<br><br>");
-		input = input.replaceAll("\\s\\s", "<br>");
+		HashMap<String, String> convertMap = Constants.toHTMLMap();
+		for (Map.Entry m : convertMap.entrySet()) {
+			input = input.replaceAll(m.getKey().toString(), m.getValue().toString());
+		}
 		converted = input;
 		return converted;
 	}
 	
-	
+	/**
+	 * Given input string it will remove HTML tags. If cleanup is null, it will use
+	 * the static map in Constants. 
+	 * @param input String to be cleaned up
+	 * @param cleanup not required. 
+	 * @return clean String which is also trimmed 
+	 * @throws Exception
+	 */
+	@SuppressWarnings("rawtypes")
+	public String cleanHTMLTagsAndEncoding(String input, HashMap<String, String> cleanup) throws Exception{
+		String clean = "";
+		
+		HashMap<String, String> convertMap = null;
+		if(cleanup == null) {
+			convertMap = Constants.cleanupMap();
+		}else {
+			convertMap = cleanup;
+		}
+		
+		//Look for HTML tags and remove them 
+		input = input.replaceAll("(\\<)(.*?)(\\>)", " ");
 
+		for (Map.Entry m : convertMap.entrySet()) {
+			input = input.replaceAll(m.getKey().toString(), m.getValue().toString());
+		}
+		clean = input;
+		return clean.trim();
+	}
+
+	public int wordCount (String inputTextOrFilePath)throws Exception{
+		int count = 0;
+		if (inputTextOrFilePath != null) {
+			IO_Utils io = new IO_Utils();
+			if (io.fileExists(inputTextOrFilePath)) {
+				log.debug("Will load the file");
+				count = new StringTokenizer(io.fileToString(inputTextOrFilePath)).countTokens();
+			}else {
+				log.debug("Not a file, will use String");
+				count = new StringTokenizer(inputTextOrFilePath).countTokens();
+			}
+
+			return count;	
+		}else {
+			return 0;
+		}
+	}
+
+	public String stringArrayToParagraph(String[] sentences) {
+		String paragraph = "";
+		if (sentences != null && sentences.length > 0) {
+			for (String sentence : sentences) {
+				paragraph += sentence + "\n\n";
+			}
+		}
+		return paragraph;
+	}
 }
