092a2f1c807ee5b406d904cece50483a7cf6b5df
Ivan Alonso <ivan@alonso.tv>
2018-03-14 19:28:25 +0100

    Included a new method to build URLs

--- src/main/java/co/alonsos/java_utilities/net_http/HTTP_Helper.java
+++ src/main/java/co/alonsos/java_utilities/net_http/HTTP_Helper.java
@@ -2,7 +2,11 @@ package co.alonsos.java_utilities.net_http;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 
 public class HTTP_Helper {
@@ -15,5 +19,29 @@ public class HTTP_Helper {
 		}catch(UnsupportedEncodingException uee){
 			throw new IllegalArgumentException(uee);
 		}
+	}
+	
+	public static String urlBuilder(String host, int port, String path, List<NameValuePair> query, boolean SSL) throws Exception{
+		URIBuilder url = new URIBuilder();
+		try {
+			if (host != null) {
+				url.setHost(host);
+			}else {
+				url.setHost("localhost");
+			}
+			url.setPort(port);
+			url.setPath(path);
+			if (query != null) {
+				url.addParameters(query);
+			}
+			if (SSL) {
+				url.setScheme("https");
+			}else {
+				url.setScheme("http");
+			}
+		}catch(Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		return url.toString();
 	}
 }
--- src/test/java/co/alonsos/java_utilities/net_http/HTTP_HelperTest.java
+++ src/test/java/co/alonsos/java_utilities/net_http/HTTP_HelperTest.java
@@ -1,20 +1,74 @@
 package co.alonsos.java_utilities.net_http;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import co.alonsos.java_utilities.net_http.HTTP_Helper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class HTTP_HelperTest {
-	
-	String host = "loalhost.com";
-	int port = 1234;
-	String api = "api";
 	
 	@Test
 	public void testURLEncoder() {
 		String input = "https://localhost.com:1234/api?query1=value2&query2=value2";
 		String expected = "https%3A%2F%2Flocalhost.com%3A1234%2Fapi%3Fquery1%3Dvalue2%26query2%3Dvalue2";
 		Assert.assertEquals(expected, HTTP_Helper.encodeUrl(input));
+	}
+	
+	@Test
+	public void testURLBuilder() throws Exception {
+		String expected = "https://localhost.com:1234/api?query1=value1&query2=value2";
+		String host = "localhost.com";
+		int port = 1234;
+		String path = "api";
+		List<NameValuePair> q = new ArrayList<NameValuePair>();
+		q.add(new BasicNameValuePair("query1", "value1"));
+		q.add(new BasicNameValuePair("query2", "value2"));
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, port, path, q, true));
+	}
+	
+	@Test
+	public void testURLBuilderNullPath() throws Exception {
+		String expected = "https://localhost.com:1234?query1=value1&query2=value2";
+		String host = "localhost.com";
+		int port = 1234;
+		List<NameValuePair> q = new ArrayList<NameValuePair>();
+		q.add(new BasicNameValuePair("query1", "value1"));
+		q.add(new BasicNameValuePair("query2", "value2"));
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, port, null, q, true));
+	}
+	
+	@Test
+	public void testURLBuilderNullQuery() throws Exception {
+		String expected = "https://localhost.com:1234/api";
+		String host = "localhost.com";
+		int port = 1234;
+		String path = "api";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, port, path, null, true));
+	}
+	
+	@Test
+	public void testURLBuilderNoSSL() throws Exception {
+		String expected = "http://localhost.com:1234/api?query1=value1&query2=value2";
+		String host = "localhost.com";
+		int port = 1234;
+		String path = "api";
+		List<NameValuePair> q = new ArrayList<NameValuePair>();
+		q.add(new BasicNameValuePair("query1", "value1"));
+		q.add(new BasicNameValuePair("query2", "value2"));
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, port, path, q, false));
+	}
+	
+	@Test
+	public void testURLBuilderNullHost() throws Exception {
+		String expected = "https://localhost:1234/api";
+		int port = 1234;
+		String path = "api";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(null, port, path, null, true));
 	}
 }
