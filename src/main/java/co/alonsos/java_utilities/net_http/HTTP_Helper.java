5d653b9f03acb0298e1db9f80b1310937b0d6feb
Ivan Alonso <ivan@alonso.tv>
2018-03-15 10:39:56 +0100

    Included new method for custom URL paths

--- src/main/java/co/alonsos/java_utilities/net_http/HTTP_Helper.java
+++ src/main/java/co/alonsos/java_utilities/net_http/HTTP_Helper.java
@@ -3,16 +3,18 @@ package co.alonsos.java_utilities.net_http;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
-
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 
 public class HTTP_Helper {
 	private static Logger log = Logger.getLogger(HTTP_Helper.class);
 
-	
+	/**
+	 * Given a URL, it will return a UTF-8 encoded String
+	 * @param url: Encoded String
+	 * @return
+	 */
 	public static String encodeUrl(String url){
 		try{
 			return URLEncoder.encode(url, "UTF-8");
@@ -21,6 +23,16 @@ public class HTTP_Helper {
 		}
 	}
 	
+	/**
+	 * Utilizes the URIBuilder class from Apache to build a URL and return as a string
+	 * @param host: Only include the host like loalhost.com
+	 * @param port: Only include the port like 8080
+	 * @param path: Only include the path to of the service, like /v1/path/something/
+	 * @param query: This is a KV pair of queires and values
+	 * @param SSL: If true it will use HTTPS
+	 * @return
+	 * @throws Exception
+	 */
 	public static String urlBuilder(String host, int port, String path, List<NameValuePair> query, boolean SSL) throws Exception{
 		URIBuilder url = new URIBuilder();
 		try {
@@ -43,5 +55,40 @@ public class HTTP_Helper {
 			log.error(e.getMessage(), e);
 		}
 		return url.toString();
+	}
+	
+	/**
+	 * Does NOT use URIBuilder, it just strings the path together the parameters into a URL
+	 * Unless necessary, you should use the other option
+	 * @param host: Only include the host like loalhost.com
+	 * @param fullPath: Include the path and any query parameters you need
+	 * @param APIKey: Only include the actual key, the method will add the "apiKey" query
+	 * @param SSL: If true it will use HTTPS
+	 * @return
+	 */
+	public static String urlBuilder(String host, String fullPath, String APIKey, boolean SSL) {
+		String fullURL = "";
+		String apiKeyQuery = "&apiKey=";
+		if (SSL) {
+			fullURL = "https://";
+		}else {
+			fullURL = "http://";
+		}
+		
+		if (host == null || host.isEmpty()) {
+			host = "localhost";
+		}
+		
+		if (fullPath == null || fullPath.isEmpty()) {
+			apiKeyQuery = "?apiKey=";
+			fullPath = "/";
+		}
+		
+		if(APIKey == null) {
+			APIKey = "";
+		}
+		
+		fullURL += host + fullPath + apiKeyQuery + APIKey;
+		return fullURL;
 	}
 }
--- src/test/java/co/alonsos/java_utilities/net_http/HTTP_HelperTest.java
+++ src/test/java/co/alonsos/java_utilities/net_http/HTTP_HelperTest.java
@@ -33,6 +33,18 @@ public class HTTP_HelperTest {
 	}
 	
 	@Test
+	public void testURLBuilderLongPath() throws Exception {
+		String expected = "https://localhost.com:1234/v1/api?query1=value1&query2=value2";
+		String host = "localhost.com";
+		int port = 1234;
+		String path = "v1/api";
+		List<NameValuePair> q = new ArrayList<NameValuePair>();
+		q.add(new BasicNameValuePair("query1", "value1"));
+		q.add(new BasicNameValuePair("query2", "value2"));
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, port, path, q, true));
+	}
+	
+	@Test
 	public void testURLBuilderNullPath() throws Exception {
 		String expected = "https://localhost.com:1234?query1=value1&query2=value2";
 		String host = "localhost.com";
@@ -70,5 +82,45 @@ public class HTTP_HelperTest {
 		int port = 1234;
 		String path = "api";
 		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(null, port, path, null, true));
+	}
+	
+	@Test
+	public void testURLBuilderCustom()throws Exception {
+		String expected = "https://localhost.com/v2/everything?domains=nationalgeographic.com&apiKey=longApiKey123";
+		String host = "localhost.com";
+		String path = "/v2/everything?domains=nationalgeographic.com";
+		String APIKey = "longApiKey123";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, path, APIKey, true));
+	}
+	
+	@Test
+	public void testURLBuilderCustomNoSSL()throws Exception {
+		String expected = "http://localhost.com/v2/everything?domains=nationalgeographic.com&apiKey=longApiKey123";
+		String host = "localhost.com";
+		String path = "/v2/everything?domains=nationalgeographic.com";
+		String APIKey = "longApiKey123";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(host, path, APIKey, false));
+	}
+	
+	@Test
+	public void testURLBuilderCustomNoHost()throws Exception {
+		String expected = "http://localhost/v2/everything?domains=nationalgeographic.com&apiKey=longApiKey123";
+		String path = "/v2/everything?domains=nationalgeographic.com";
+		String APIKey = "longApiKey123";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(null, path, APIKey, false));
+	}
+	
+	@Test
+	public void testURLBuilderCustomNoPath()throws Exception {
+		String expected = "http://localhost/?apiKey=longApiKey123";
+		String APIKey = "longApiKey123";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(null, null, APIKey, false));
+	}
+	
+	@Test
+	public void testURLBuilderCustomNoKey()throws Exception {
+		String expected = "http://localhost/v2/everything?domains=nationalgeographic.com&apiKey=";
+		String path = "/v2/everything?domains=nationalgeographic.com";
+		Assert.assertEquals(expected, HTTP_Helper.urlBuilder(null, path, null, false));
 	}
 }
