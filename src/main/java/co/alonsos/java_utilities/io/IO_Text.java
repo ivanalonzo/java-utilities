6775232b90a19793fee7f1cab0d913c1d0cf3f26
Ivan Alonso <ivan@alonso.tv>
2018-05-29 14:54:34 +0200

    Added double newlines support to change it to paragraphs

--- src/main/java/co/alonsos/java_utilities/io/IO_Text.java
+++ src/main/java/co/alonsos/java_utilities/io/IO_Text.java
@@ -77,6 +77,8 @@ public class IO_Text {
 			clean = clean.replaceAll("\\\\n", "");
 			clean = clean.replaceAll("\\n", "");
 		}else {
+			clean = clean.replaceAll("\\\\n\\\\n", "</p>");
+			clean = clean.replaceAll("\\n\\n", "</p>");
 			clean = clean.replaceAll("\\\\n", "</p>");
 			clean = clean.replaceAll("\\n", "</p>");
 		}
